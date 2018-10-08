185412419f614067b2be83053034dff453dab79d
marcusp619 <marcusp619@gmail.com>
2018-10-08 16:59:55 -0600

    Add enzyme to package.json

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+var enzyme = require('enzyme');
+
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = enzyme.shallow(<App />);
+
+   expect(wrapper).toMatchSnapshot();
 });

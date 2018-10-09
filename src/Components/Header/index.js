f8a21e3deb9fc89cecbf7e39999181a4be739e3e
marcusp619 <marcusp619@gmail.com>
2018-10-08 19:49:14 -0600

    Add initial structure for Header component

---
+++ src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends from Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    <div className="Header">
+      <h2>Header Component</h2>
+    </div>
+  }
+}

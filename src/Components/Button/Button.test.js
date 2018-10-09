ade7727248fc7676331c12725029d79d02ee4e52
marcusp619 <marcusp619@gmail.com>
2018-10-08 19:58:15 -0600

    Add initial structure for Button component

--- src/Components/Button/Button.test.js
+++ src/Components/Button/Button.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header;
+import Button from './Button;
 const enzyme = require('enzyme');
 
 it('renders without crashing', () => {
-  const wrapper = enzyme.shallow(<Header />);
+  const wrapper = enzyme.shallow(<Button />);
 
    expect(wrapper).toMatchSnapshot();
 });
---
+++ src/Components/Button/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends from Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    <div className="Button">
+      <h2>Button Component</h2>
+    </div>
+  }
+}

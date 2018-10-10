3b433148e6b1a269031ef1f4fd4e9c372ac600b1
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-10-09 18:33:18 -0600

    Add scrolling text

--- src/Components/Movie/index.js
+++ src/Components/Movie/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Movie.css';
+import './StarWarsIntro.css'
 
 class Movie extends Component {
   constructor(props) {
@@ -8,7 +9,24 @@ class Movie extends Component {
 
   render() {
     return(
-      <p>{this.props.randomFilmText}</p>
+      <div class="star-wars-intro">
+        <p class="intro-text">
+          A few days ago, during...
+        </p>   
+        <h2 class="main-logo">
+          <img src="img/star-wars-intro.png" />
+        </h2>
+      <div class="main-content">
+        <div class="title-content">
+          <p class="content-header">MOVIE TITLE: EPISODE WHATEVER</p>
+          <br />
+          <p class="content-body">
+           {this.props.randomFilmText}
+          </p>
+        </div>
+  </div>
+</div>
+
     )
   }
 }

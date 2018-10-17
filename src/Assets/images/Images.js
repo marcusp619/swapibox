fb3c35e4228501eea566366eb3864aadf7575a23
marcusp619 <marcusp619@gmail.com>
2018-10-16 18:51:56 -0600

    Add imgFinder function

--- src/Assets/images/Images.js
+++ src/Assets/images/Images.js
@@ -1,12 +1,12 @@
-module.exports = {
-  'Luke Skywalker': 'src/Assets/images/beru-lars.jpg',
-  'C-3PO': 'src/Assets/images/c-3Po.jpg',
-  'R2-D2': 'src/Assets/images/r2-d2.jpg',
-  'Darth Vader': 'src/Assets/images/darth-vader.jpg',
-  'Leia Organa': 'src/Assets/images/leia-organa.jpg',
-  'Owen Lars': 'src/Assets/images/owen-lars.jpg',
-  'Beru Whitesun lars': 'src/Assets/images/beru-lars.jpg',
-  'R5-D4': 'src/Assets/images/r5-d4.jpg',
-  'Biggs Darklighter': '/Users/markpearyer/Documents/Turing/swapibox/src/Assets/images/biggs-darklighter.jpg',
-  'Obi-Wan Kenobi': 'src/Assets/images/obi-wan-kenobi.jpg'
-};
+module.exports = [
+  { 'Luke Skywalker': '../../Assets/images/luke-skywalker.jpg' },
+  { 'C-3PO': './c-3Po.jpg' },
+  { 'R2-D2': 'src/Assets/images/r2-d2.jpg' },
+  { 'Darth Vader': 'src/Assets/images/darth-vader.jpg' },
+  { 'Leia Organa': 'src/Assets/images/leia-organa.jpg' },
+  { 'Owen Lars': 'src/Assets/images/owen-lars.jpg' },
+  { 'Beru Whitesun lars': 'src/Assets/images/beru-lars.jpg' },
+  { 'R5-D4': 'src/Assets/images/r5-d4.jpg' },
+  { 'Biggs Darklighter': 'src/Assets/images/biggs-darklighter.jpg' },
+  { 'Obi-Wan Kenobi': 'src/Assets/images/obi-wan-kenobi.jpg' },
+];
--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import * as Images from '../../Assets/images/Images';
 import './Card.css';
 
+const ImgFinder = (name) => {
+  const image = Images.find(person => person[name]);
+  const results = Object.values(image);
+  return results;
+};
+
+
 const Card = ({homeworld, name, species}) => {
+  const img = ImgFinder(name);
   return (
     <div className="card">
       <div className="card-img">
+        <img src={img} alt={name} />
       </div>
       <div className="card-body">
         <h5>{name}</h5>
